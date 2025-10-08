acf3559b764162e9351b840243eb3c4145926222
miguelferreira1985 <miguel.fermo1985@gmail.com>
2025-10-07 21:17:33 -0600

    fix issue with collapsed menu

--- src/app/components/layouts/main-layout/main-layout.ts
+++ src/app/components/layouts/main-layout/main-layout.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { RouterLink } from "@angular/router";
 import { HasRoleDirective } from '../../../core/has-role.directive';
+
+declare const $: any; 
 
 @Component({
   selector: 'app-main-layout',
@@ -15,9 +17,22 @@ import { HasRoleDirective } from '../../../core/has-role.directive';
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements AfterViewInit {
 
   constructor(private authService: AuthService) {}
+
+  ngAfterViewInit(): void {
+    // 🔁 Esperar a que la vista esté lista y luego inicializar los menús colapsables
+    this.initializeSidebar();
+  }
+
+  private initializeSidebar(): void {
+    setTimeout(() => {
+      // Esto re-inicializa el treeview de AdminLTE
+      $('[data-widget="treeview"]').Treeview('init');
+    }, 200);
+  }
+
 
   logout(): void {
     this.authService.logout();
